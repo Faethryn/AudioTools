eeac247d986bb6f7040abbc8f79f51bb5dffeac0
Faethryn <laurens.muylle@student.howest.be>
2023-11-03 16:24:24 +0100

    Added a new version of the audiomanager

--- Assets/Scripts/Audio/AudioManager.cs
+++ Assets/Scripts/Audio/AudioManager.cs
@@ -1,9 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class AudioManager : MonoBehaviour
 {
+
+    #region OneShots
     [SerializeField] 
     private AudioSourceSettings _defaultAudioSourceSettings;
 
@@ -47,9 +50,67 @@ public class AudioManager : MonoBehaviour
     }
 
 
+    #endregion
+
+    #region MusicPlayback
+    [SerializeField]
+    private AudioSourceSettings _defaultMusicSourceSettings;
+
+    private MusicSection _currentMusicSection = null;
+
+    public void PlayMusic(MusicSection section)
+    {
+        AudioSource musicSource = FindMusicSource();
+
+       _defaultMusicSourceSettings.SetSourceSettings(musicSource);
+
+        section.PlaySection(musicSource);
+        _currentMusicSection = section;
+
+       StartCoroutine( PlayNextSectionDelayed(_currentMusicSection));
+
+    }
+
+    public void OverrideMusic(MusicSection section)
+    {
+        _currentMusicSection.OverrideSection(section);
+       
+    }
+
+
+
+
+    private IEnumerator PlayNextSectionDelayed(MusicSection currentSection)
+    {
+
+        yield return new WaitForSecondsRealtime(currentSection._length);
+
+        if (currentSection._overrideSection != null)
+        {
+            PlayMusic(currentSection._overrideSection);
+            currentSection._overrideSection = null;
+        }
+        else
+        {
+            PlayMusic(currentSection._nextSection);
+            currentSection._overrideSection = null;
+        }
+
+       
+    }
+
+
+    #endregion
+
+
+
+
+    #region Initiation
+
     private List<AudioSource> _sources = new List<AudioSource>();
 
-   
+    [SerializeField] 
+    private List<AudioSource> _musicSources = new List<AudioSource>();
 
     public void Awake( )
     {
@@ -58,8 +119,20 @@ public class AudioManager : MonoBehaviour
         {
             GameObject newAudioSource = Instantiate(tempObject, this.transform);
 
-            _sources.Add(newAudioSource.AddComponent<AudioSource>());      
-            
+            _sources.Add(newAudioSource.AddComponent<AudioSource>());
+
+          
+
+        }
+
+        for (int i = 0; i < 3; i++)
+        {
+            GameObject newAudioSource = Instantiate(tempObject, this.transform);
+
+            _musicSources.Add(newAudioSource.AddComponent<AudioSource>());
+
+
+
         }
 
         Destroy( tempObject );
@@ -98,4 +171,41 @@ public class AudioManager : MonoBehaviour
         Destroy( newAudioSource );
         return _sources[_sources.Count - 1];
     }
+
+
+    private AudioSource FindMusicSource()
+    {
+        bool foundSource = false;
+
+        int index = 0;
+
+        while (foundSource == false && index < _musicSources.Count)
+        {
+            if (_musicSources[index].isPlaying == false)
+            {
+                foundSource = true;
+                return _musicSources[index];
+
+            }
+            else
+            {
+                index++;
+            }
+
+        }
+
+
+        GameObject newAudioSource = new GameObject();
+
+
+
+        var tempAudioSource = Instantiate(newAudioSource, this.transform);
+        _musicSources.Add(tempAudioSource.AddComponent<AudioSource>());
+
+        Destroy(newAudioSource);
+        return _musicSources[_sources.Count - 1];
+    }
+
+
+    #endregion
 }
